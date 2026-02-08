860eb662e4a77ec85e8a358377050381277ab7ba
Abhijith Sunder <abhijithsunder01@gmail.com>
2026-02-08 18:54:14 +0530

    add about page

--- client.ts
+++ client.ts
@@ -1,8 +1,11 @@
+import aboutpage from "./public/about.html";
 import articlepage from "./public/article.html";
 import homepage from "./public/index.html";
+
 const routes = {
   "/": homepage,
   "/article/:slug": articlepage,
+  "/about": aboutpage,
 };
 
 export { routes };
