4ad601d3711f624a544801c7442e6464ab71f2d0
Abhijith Sunder <abhijithsunder01@gmail.com>
2026-02-14 16:09:23 +0530

    add homepage

--- client.ts
+++ client.ts
@@ -1,11 +1,13 @@
 import aboutpage from "./public/about.html";
 import articlepage from "./public/article.html";
+import feedpage from "./public/feed.html";
 import homepage from "./public/index.html";
 
 const routes = {
   "/": homepage,
   "/article/:slug": articlepage,
   "/about": aboutpage,
+  "/feed": feedpage,
 };
 
 export { routes };
